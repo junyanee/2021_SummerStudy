711426fc8b31a2c77dc5e81f357266967b63d208
박준현 <grssa@naver.com>
2021-07-19 22:18:54 +0900

    CodeUp100제

---
+++ CodeUp100/6097.py
@@ -0,0 +1,21 @@
+h, w = map(int, input().split())
+n = int(input())
+board = []
+for i in range(h):
+    board.append([])
+    for j in range(w):
+        board[i].append(0)
+
+for i in range(n):
+    l, d, x, y = map(int, input().split())
+    if d == 0:
+        for j in range(l):
+            board[x-1][y-1+j] = 1
+    else:
+        for j in range(l):
+            board[x-1+j][y-1] = 1
+
+for i in range(h):
+    for j in range(w):
+        print(board[i][j], end=' ')
+    print()
