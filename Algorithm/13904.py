6b1b0b126732b2fcb1abb36f9204b74cfd017ef9
박준현 <grssa@naver.com>
2021-07-27 00:13:53 +0900

    13904
    백준 13904
    과제

--- Algorithm/13904.py
+++ Algorithm/13904.py
@@ -1,3 +1,25 @@
 # 백준 13904
 # 과제
 
+n = int(input())
+homeworks = []
+for i in range(n):
+    d, w = map(int, input().split())
+    homeworks.append((d,w))
+homeworks.sort()
+
+todo = []
+date = homeworks[-1][0]
+ans = 0
+
+while True:
+    if date == 0:
+        break
+    while homeworks and homeworks[-1][0] >= date:
+        todo.append(homeworks.pop()[1])
+    date -= 1
+    if not todo:
+        continue
+    todo.sort()
+    ans += todo.pop()
+print(ans)
