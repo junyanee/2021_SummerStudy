6b03869a3115a92b9d52ba52dd5e35a740d451c2
박준현 <grssa@naver.com>
2021-08-14 23:20:16 +0900

    11256
    백준 11256
    사탕

--- Algorithm/12931.py
+++ Algorithm/12931.py
@@ -1,2 +1,22 @@
-# 백준 12931
-# 두 배 더하기
+# 백준 11256
+# 사탕
+
+T = int(input())
+for t in range(T):
+    J, N = map(int, input().split())
+
+    boxes = []
+    for box_num in range(N):
+        Ri, Ci = map(int, input().split())
+        box = Ri * Ci
+        boxes.append(box)
+
+    boxes.sort(reverse=True)
+
+    candy = 0
+    for box_index in range(len(boxes)):
+        candy += boxes[box_index]
+
+        if candy >= J:
+            print(box_index + 1)
+            break
