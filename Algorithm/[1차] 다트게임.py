b6e02d7774843593fefe38334c28f082f27543be
박준현 <grssa@naver.com>
2021-08-23 23:21:15 +0900

    [1차] 다트게임
    프로그래머스
    [1차] 다트게임

---
+++ Algorithm/[1차] 다트게임.py
@@ -0,0 +1,36 @@
+# 프로그래머스
+# [1차] 다트게임
+
+dartResult = list(input())
+score = []
+n = ''
+
+for i in dartResult:
+    if i.isnumeric():
+        n += i
+    elif i == 'S':
+        n = int(n) ** 1
+        score.append(n)
+        n = ''
+    elif i == 'D':
+        n = int(n) ** 2
+        score.append(n)
+        n = ''
+    elif i == 'T':
+        n = int(n) ** 3
+        score.append(n)
+        n = ''
+    elif i == '*':
+        if len(score) > 1:
+            score[-2] = score[-2] * 2
+            score[-1] = score[-1] * 2
+        else:
+            score[-1] = score[-1] * 2
+    elif i == '#':
+        score[-1] = score[-1] * -1
+
+
+print(sum(score))
+
+
+
